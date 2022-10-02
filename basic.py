a2458002b33d410bc5b136930a9cb24fe6a88c7f
Guilherme Alencar <guilhermesa151@hotmail.com>
2022-10-01 23:20:24 -0300

    minor refector in main.py to basic.py

--- basic.py
+++ basic.py
@@ -2,16 +2,16 @@ import cv2
 import numpy as np
 import face_recognition
 
-imgElon = face_recognition.load_image_file('ImageBasic/guilherme.jpeg')
-imgElon = cv2.cvtColor(imgElon, cv2.COLOR_BGR2RGB)
+imgGuilherme = face_recognition.load_image_file('ImageBasic/guilherme.jpeg')
+imgGuilherme = cv2.cvtColor(imgGuilherme, cv2.COLOR_BGR2RGB)
 
 imgTest = face_recognition.load_image_file('ImageBasic/Bill gates.jpg')
 imgTest = cv2.cvtColor(imgTest, cv2.COLOR_BGR2RGB)
 
 # Finding faces and Encoding
-faceLoc = face_recognition.face_locations(imgElon)[0] # top_right, bottom_right, top_left, bottom_left
-encodeElon = face_recognition.face_encodings(imgElon)[0]
-cv2.rectangle(imgElon, (faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]), (255, 0, 255), 2)
+faceLoc = face_recognition.face_locations(imgGuilherme)[0] # top_right, bottom_right, top_left, bottom_left
+encodeElon = face_recognition.face_encodings(imgGuilherme)[0]
+cv2.rectangle(imgGuilherme, (faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]), (255, 0, 255), 2)
 
 faceLocTest = face_recognition.face_locations(imgTest)[0] # top_right, bottom_right, top_left, bottom_left
 encodeTest = face_recognition.face_encodings(imgTest)[0]
@@ -23,5 +23,5 @@ print(results, faceDis)
 cv2.putText(imgTest, f'{results} {round(faceDis[0],2)}', (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
 
 cv2.imshow('Elon Test', imgTest)
-cv2.imshow('Elon Musk', imgElon)
+cv2.imshow('Elon Musk', imgGuilherme)
 cv2.waitKey(0)
