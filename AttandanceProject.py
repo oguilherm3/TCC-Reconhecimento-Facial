0faa033daa6be5f4e89a287b16f72e9fe2e48aa9
Guilherme Alencar <guilhermesa151@hotmail.com>
2022-10-08 21:44:51 -0300

    fixing typos

--- AttandanceProject.py
+++ AttandanceProject.py
@@ -3,7 +3,7 @@ import numpy as np
 import face_recognition
 import os
 
-path = 'ImagesAttandance'
+path = 'ImagesAttendance'
 images = []
 classNames = []
 myList = os.listdir(path)
@@ -28,11 +28,11 @@ print('Encoding complete')
 cap = cv2.VideoCapture(0)
 
 while True:
-    sucess, img = cap.read()
+    success, img = cap.read()
     imgS = cv2.resize(img, (0,0), None, 0.25, 0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
-    facesCurFrame = face_recognition.face_locations(imgS) #find all locations in small image
+    facesCurFrame = face_recognition.face_locations(imgS)                  #find all locations in small image
     encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame) #send the faces & its locations
 
     for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
@@ -53,13 +53,13 @@ while True:
 
     cv2.imshow('Webcam', img)
     cv2.waitKey(1)
-#faceLoc = face_recognition.face_locations(imgElon)[0] # top_right, bottom_right, top_left, bottom_left
-#encodeElon = face_recognition.face_encodings(imgElon)[0]
-#cv2.rectangle(imgElon, (faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]), (255, 0, 255), 2)
+#faceLoc = face_recognition.face_locations(imgGuilherme)[0] # top_right, bottom_right, top_left, bottom_left
+#encodeGuilherme = face_recognition.face_encodings(imgGuilherme)[0]
+#cv2.rectangle(imgGuilherme, (faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]), (255, 0, 255), 2)
 
 #faceLocTest = face_recognition.face_locations(imgTest)[0] # top_right, bottom_right, top_left, bottom_left
 #encodeTest = face_recognition.face_encodings(imgTest)[0]
 #cv2.rectangle(imgTest, (faceLocTest[3], faceLocTest[0]), (faceLocTest[1], faceLocTest[2]), (255, 0, 255), 2)
 
-#results = face_recognition.compare_faces([encodeElon], encodeTest)
-#faceDis = face_recognition.face_distance([encodeElon], encodeTest)
+#results = face_recognition.compare_faces([encodeGuilherme], encodeTest)
+#faceDis = face_recognition.face_distance([encodeGuilherme], encodeTest)
