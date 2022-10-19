94836c4643d32427257e570c06f077fe6a05fd78
Guilherme Alencar <guilhermesa151@hotmail.com>
2022-10-18 23:46:31 -0300

    Creation of the Model

---
+++ Model/Aluno.py
@@ -0,0 +1,21 @@
+from Model.Pessoa import Pessoa
+
+
+class Aluno(Pessoa):
+    def __init__(self, nome, rg, cpf, face):  # Aluno Constructor
+        super().__init__(nome, rg, cpf)  # Pessoa Constructor
+        self.course = ''
+        self.campus = ''
+        self.cep = ''
+        self.address = ''
+        self.complement = ''
+        self.face = face
+        self.phone = ''
+        self.raGenerator()
+        self.findFace()
+
+    def raGenerator(self):
+        pass
+
+    def findFace(self):
+        pass
---
+++ Model/Pessoa.py
@@ -0,0 +1,10 @@
+class Pessoa:
+    def __init__(self, nome, rg, cpf):
+        self.name = nome
+        self.rg = rg
+        self.cpf = cpf
+        self.birthDate = ''
+
+    def verifyCpf(self):
+        pass
+
